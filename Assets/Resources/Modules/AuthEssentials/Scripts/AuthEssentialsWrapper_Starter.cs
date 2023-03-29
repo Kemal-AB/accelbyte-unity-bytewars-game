8e6777485f1cf7ee2fc3de7ad6a939f3de14e07a
Kemal Maulidiasani <kemal.maulidiasani@accelbyte.net>
2023-03-29 08:16:06 +0000

    fix: improve module 2 with starter files
    * fix: improve module 2 with starter files

---
+++ Assets/Resources/Modules/AuthEssentials/Scripts/AuthEssentialsWrapper_Starter.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using AccelByte.Api;
+using AccelByte.Core;
+using AccelByte.Models;
+using UnityEngine;
+
+public class AuthEssentialsWrapper_Starter : MonoBehaviour
+{
+    //Copy AccelByte's Multi Registry references from "Use the AccelByte SDK to Login" unit here (step number 2)
+    
+
+    //paste platform type code from "Use the AccelByte SDK to Login" here (step number 4)
+    
+    
+    //Copy Start() function from "Use the AccelByte SDK to Login" unit here (step number 2)
+
+    
+    //Paste Login() function from "Use the AccelByte SDK to Login" here (step number 5)
+
+
+    //Paste OnloginCompleted here from "Use the AccelByte SDK to Login" unit here (step number 3)
+
+    
+}
