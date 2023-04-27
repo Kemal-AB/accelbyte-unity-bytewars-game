249470db0e16b8142aa270404fd9f2c5022cc93a
Nathania Saphira <nathania@accelbyte.net>
2023-04-27 09:33:46 +0700

    feat(cloud save): add player record implementation for volume settings

---
+++ Assets/Resources/Modules/CloudSaveEssentials/Scripts/CloudSaveEssentialsWrapper.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using AccelByte.Api;
+using AccelByte.Core;
+using AccelByte.Models;
+using UnityEngine;
+
+public class CloudSaveEssentialsWrapper : MonoBehaviour
+{
+    // AccelByte's Multi Registry references
+    private CloudSave cloudSave;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cloudSave = MultiRegistry.GetApiClient().GetCloudSave();
+    }
+
+    #region AB Service Functions
+
+    public void SaveUserRecord(string recordKey, Dictionary<string, object> recordRequest, ResultCallback resultCallback)
+    {
+        cloudSave.SaveUserRecord(
+            recordKey, 
+            recordRequest, 
+            result => OnSaveUserRecordCompleted(result, resultCallback)
+        );
+    }
+
+    public void GetUserRecord(string recordKey, ResultCallback<UserRecord> resultCallback)
+    {
+        cloudSave.GetUserRecord(
+            recordKey,
+            result => OnGetUserRecordCompleted(result, resultCallback)
+        );
+    }
+    
+    #endregion
+
+    #region Callback Functions
+
+    private void OnSaveUserRecordCompleted(Result result, ResultCallback customCallback = null)
+    {
+        if (!result.IsError)
+        {
+            Debug.Log("Save Player Record from Client successful.");
+        }
+        else
+        {
+            Debug.Log($"Save Player Record from Client failed. Message: {result.Error.Message}");
+        }
+        
+        customCallback?.Invoke(result);
+    }
+    
+    private void OnGetUserRecordCompleted(Result<UserRecord> result, ResultCallback<UserRecord> customCallback = null)
+    {
+        if (!result.IsError)
+        {
+            Debug.Log("Save Player Record from Client successful.");
+        }
+        else
+        {
+            Debug.Log($"Save Player Record from Client failed. Message: {result.Error.Message}");
+        }
+        
+        customCallback?.Invoke(result);
+    }
+
+    #endregion
+}
--- Assets/Scripts/UI/MainMenu/HelpAndOptions/Options/OptionsMenu.cs
+++ Assets/Scripts/UI/MainMenu/HelpAndOptions/Options/OptionsMenu.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using AccelByte.Core;
+using AccelByte.Models;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -12,8 +15,25 @@ public class OptionsMenu : MenuCanvas
     [SerializeField] private TMP_Text sfxVolumeText;
     [SerializeField] private Button backButton;
 
+    private CloudSaveEssentialsWrapper _cloudSaveWrapper = new CloudSaveEssentialsWrapper();
+    private Dictionary<string, object> volumeSettings;
+
+    // player record key and configurations
+    private const string PLAYERSETTINGS_RECORDKEY = "PLAYER_SETTINGS";
+    private const string MUSICVOLUME_RECORDNAME = "music-volume";
+    private const string SFXVOLUME_RECORDNAME = "sfx-volume"; 
+
     void Start()
     {
+        // get cloud save's wrapper
+        _cloudSaveWrapper = TutorialModuleManager.Instance.GetModuleClass<CloudSaveEssentialsWrapper>();
+        // Volume Settings dictionary initialization
+        volumeSettings = new Dictionary<string, object>()
+        {
+            {MUSICVOLUME_RECORDNAME, 0},
+            {SFXVOLUME_RECORDNAME, 0}
+        };
+        
         // Initialize options value based on PlayerPrefs stored in AudioManager
         musicVolumeSlider.value = AudioManager.Instance.GetCurrentVolume(AudioManager.AudioType.MusicAudio);
         sfxVolumeSlider.value = AudioManager.Instance.GetCurrentVolume(AudioManager.AudioType.SfxAudio);
@@ -26,12 +46,19 @@ public class OptionsMenu : MenuCanvas
         backButton.onClick.AddListener(() => MenuManager.Instance.OnBackPressed());
     }
 
+    void OnEnable()
+    {
+        _cloudSaveWrapper.GetUserRecord(PLAYERSETTINGS_RECORDKEY, OnGetUserRecordCompleted);
+    }
+
     private void ChangeMusicVolume(float musicVolume)
     {
         AudioManager.Instance.SetMusicVolume(musicVolume);
         
         int musicVolumeInt = (int)(musicVolume * 100);
         musicVolumeText.text = musicVolumeInt.ToString() + "%";
+        
+        SavePlayerSettings(musicVolume, MUSICVOLUME_RECORDNAME);
     }
     
     private void ChangeSfxVolume(float sfxVolume)
@@ -40,6 +67,42 @@ public class OptionsMenu : MenuCanvas
 
         int sfxVolumeInt = (int)(sfxVolume * 100);
         sfxVolumeText.text = sfxVolumeInt.ToString() + "%";
+        
+        SavePlayerSettings(sfxVolume, SFXVOLUME_RECORDNAME);
+    }
+
+    private void SavePlayerSettings(float newVolumeValue, string recordName)
+    {
+        Debug.Log(volumeSettings[recordName]);
+        
+        float recordedVolume = (float) volumeSettings[recordName];
+        if (recordedVolume != newVolumeValue)
+        {
+            volumeSettings[recordName] = newVolumeValue;
+            
+            _cloudSaveWrapper.SaveUserRecord(PLAYERSETTINGS_RECORDKEY, volumeSettings, null);
+        }
+    }
+    
+    private void OnGetUserRecordCompleted(Result<UserRecord> result)
+    {
+        if (!result.IsError)
+        {
+            foreach (KeyValuePair<string, object> record in result.Value.value)
+            {
+                switch (record.Key)
+                {
+                    case MUSICVOLUME_RECORDNAME:
+                        volumeSettings[MUSICVOLUME_RECORDNAME] = record.Value;
+                        musicVolumeSlider.value = (float) record.Value;
+                        break;
+                    case SFXVOLUME_RECORDNAME:
+                        volumeSettings[SFXVOLUME_RECORDNAME] = record.Value;
+                        sfxVolumeSlider.value = (float) record.Value;
+                        break;
+                }
+            }
+        }
     }
 
     public override GameObject GetFirstButton()
