9d94cda0051e8c595636e22b25dcba5e93b95929
lukmanaafif <afif@accelbyte.net>
2023-06-20 18:28:22 +0700

    chore: reset the wrapper _starter file

--- Assets/Resources/Modules/SinglePlatformAuth/Scripts/SinglePlatformAuthWrapper_Starter.cs
+++ Assets/Resources/Modules/SinglePlatformAuth/Scripts/SinglePlatformAuthWrapper_Starter.cs
@@ -23,101 +23,11 @@ public class SinglePlatformAuthWrapper_Starter : MonoBehaviour
     }
     private void OnLoginWithSteamButtonClicked()
     {
-        if (loginHandler == null) return;
-        loginHandler.onRetryLoginClicked = OnLoginWithSteamButtonClicked;
-        loginHandler.SetView(LoginHandler.LoginView.LoginLoading);
-        //get steam token to be used as platform token later
-        steamHelper.GetAuthSessionTicket(OnGetAuthSessionTicketFinished);
+        // TODO: login to steam platform and pass the steam ticket over to AGS login
     }
 
     private void SetLoginWithSteamButtonClickCallback()
     {
-        if (loginHandler == null)
-        {
-            if (MenuManager.Instance != null)
-            {
-                var menuCanvas = MenuManager.Instance.GetMenu(AssetEnum.LoginMenuCanvas);
-                if (menuCanvas != null && menuCanvas is LoginHandler loginHandlerC)
-                {
-                    loginHandler = loginHandlerC;
-                    var loginWithSteamButton = loginHandler.GetLoginButton(LoginType.Steam);
-                    bool isSingleAuthModuleActive =
-                        TutorialModuleManager.Instance
-                            .IsModuleActive(TutorialType.SinglePlatformAuth);
-                    bool isLoginWithSteam = SteamManager.Initialized && isSingleAuthModuleActive;
-                    if (isLoginWithSteam)
-                    {
-                        if (GConfig.GetSteamAutoLogin())
-                        {
-                            OnLoginWithSteamButtonClicked();
-                        }
-                        else
-                        {
-                            loginWithSteamButton.onClick.AddListener(OnLoginWithSteamButtonClicked);
-                            loginWithSteamButton.gameObject.SetActive(true);
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    private void OnGetAuthSessionTicketFinished(string steamAuthSessionTicket)
-    {
-        if (loginHandler == null) return;
-        if (String.IsNullOrEmpty(steamAuthSessionTicket))
-        {
-            loginHandler.OnLoginCompleted(
-                CreateLoginErrorResult(ErrorCode.CachedTokenNotFound,
-                    "Failed to get steam token"));
-        }
-        else
-        {
-            //login using steamAuthSessionTicket
-            user.LoginWithOtherPlatform(PlatformType,
-                steamAuthSessionTicket, OnLoginWithOtherPlatformCompleted);
-        }
-    }
-    private void OnLoginWithOtherPlatformCompleted(Result<TokenData, OAuthError> result)
-    {
-        if (result.IsError)
-        {
-            loginHandler.OnLoginCompleted(result);
-        }
-        else
-        {
-            tokenData = result.Value;
-            GetUserPublicData(tokenData.user_id);
-        }
-    }
-    private void GetUserPublicData(string receivedUserId)
-    {
-        GameData.CachedPlayerState.playerId = receivedUserId;
-        user.GetUserByUserId(receivedUserId, OnGetUserPublicDataFinished);
-    }
-    private void OnGetUserPublicDataFinished(Result<PublicUserData> result)
-    {
-        if (result.IsError)
-        {
-            Debug.Log($"[{ClassName}] error OnGetUserPublicDataFinished:{result.Error.Message}");
-            loginHandler.onRetryLoginClicked = () => GetUserPublicData(tokenData.user_id);
-            loginHandler.OnLoginCompleted(CreateLoginErrorResult(result.Error.Code, result.Error.Message));
-        }
-        else
-        {
-            var publicUserData = result.Value;
-            GameData.CachedPlayerState.avatarUrl = publicUserData.avatarUrl;
-            GameData.CachedPlayerState.playerName = publicUserData.displayName;
-            loginHandler.OnLoginCompleted(Result<TokenData, OAuthError>.CreateOk(tokenData));
-        }
-    }
-
-    private Result<TokenData, OAuthError> CreateLoginErrorResult(ErrorCode errorCode, string errorDescription)
-    {
-        return Result<TokenData, OAuthError>.CreateError(new OAuthError()
-        {
-            error = errorCode.ToString(),
-            error_description = errorDescription
-        });
+        // TODO: this function will get login with steam button reference and assign onClick callback to it
     }
 }
