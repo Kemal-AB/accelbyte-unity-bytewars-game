b10818741435f0d36bc417e675dedd4d093db7b0
Nathania Saphira <nathania@accelbyte.net>
2023-07-27 09:37:29 +0700

    chore: modify asset config and starters to make sure the module using the existing leaderboard menu

---
+++ Assets/Resources/Modules/PeriodicLeaderboard/Scripts/PeriodicLeaderboardEssentialsWrapper_Starter.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AccelByte.Api;
+using AccelByte.Core;
+using AccelByte.Models;
+
+public class PeriodicLeaderboardEssentialsWrapper_Starter : MonoBehaviour
+{
+    // AccelByte's Multi Registry references
+    private Leaderboard leaderboard;
+    private Statistic statistic;
+    
+    
+}
