20c41a39f6d08bbb8c919812860b1215b68d1b24
Kemal Maulidiasani <kemal.maulidiasani@accelbyte.net>
2023-08-21 02:36:34 +0000

    fix: fix menumanager to avoid OnEnable firing twice.
    * fix: fix menumanager to avoid OnEnable firing twice.

--- Assets/Scripts/MenuManager/MenuManager.cs
+++ Assets/Scripts/MenuManager/MenuManager.cs
@@ -77,6 +77,12 @@ public class MenuManager : MonoBehaviour
     {
         if (_currentMainMenu.GetAssetEnum() != assetEnum)
         {
+            
+            if (!_menusDictionary.TryGetValue(assetEnum, out var value))
+            {
+                InstantiateCanvas(assetEnum);
+            }
+            
             LeanTween.alpha(_currentMainMenu.gameObject, 0, 0.4f).setOnComplete(() =>
             {
                 OnChangeMenuComplete(assetEnum);
@@ -91,14 +97,18 @@ public class MenuManager : MonoBehaviour
     /// <param name="assetEnum"></param>
     private void OnChangeMenuComplete(AssetEnum assetEnum)
     {
+        var targetMenu = _menusDictionary[assetEnum];
+
         if (_currentMainMenu != null)
         {
             _currentMainMenu.gameObject.SetActive(false);
         }
-
-        var targetMenu = _menusDictionary[assetEnum];
         
-        targetMenu.gameObject.SetActive(true);
+        if (!targetMenu.gameObject.activeSelf)
+        {
+            targetMenu.gameObject.SetActive(true);
+        }
+        
         _currentMainMenu = targetMenu;
         _eventSystem.SetSelectedGameObject(targetMenu.GetFirstButton());
         _mainMenusStack.Push(_currentMainMenu);
@@ -281,8 +291,10 @@ public class MenuManager : MonoBehaviour
         {
             return;
         }
-        
+
+        mainmenuConfig.starter.gameObject.SetActive(false);
         var starterMenu = Instantiate(mainmenuConfig.starter, transform);
+        mainmenuConfig.starter.gameObject.SetActive(true);
         GameObject o = starterMenu.gameObject;
         o.SetActive(false);
         string mainMenuName = mainmenuConfig.starter.gameObject.name;
@@ -292,7 +304,9 @@ public class MenuManager : MonoBehaviour
 
         foreach (var menuCanvas in mainmenuConfig.otherMenuCanvas)
         {
+            menuCanvas.gameObject.SetActive(false);
             var otherCoreMenu = Instantiate(menuCanvas, transform);
+            menuCanvas.gameObject.SetActive(true);
             otherCoreMenu.gameObject.SetActive(false);
             string gameObjectName = menuCanvas.gameObject.name;
             otherCoreMenu.name = gameObjectName;
@@ -303,13 +317,28 @@ public class MenuManager : MonoBehaviour
     private void InitMenuByModules(TutorialModuleData moduleData)
     {
         var modulePrefab = moduleData.prefab;
+        modulePrefab.gameObject.SetActive(false);
         var menubyModule = Instantiate(modulePrefab, Vector3.zero, Quaternion.identity, _instance.transform);
+        modulePrefab.gameObject.SetActive(true);
         menubyModule.name = modulePrefab.name; ;
         _menusDictionary.Add(menubyModule.GetAssetEnum(), menubyModule);
         _menusDictionary[menubyModule.GetAssetEnum()].gameObject.SetActive(false);
     }
-    
-    
+
+    public void InstantiateCanvas(AssetEnum assetEnum)
+    {
+        if (_menusDictionary.TryGetValue(assetEnum, out var value))
+        {
+            return;
+        }
+        var test = (GameObject)AssetManager.Singleton.GetAsset(assetEnum);
+        var modulePrefab = test.GetComponent<MenuCanvas>();
+        modulePrefab.gameObject.SetActive(false);
+        var menubyModule = Instantiate(modulePrefab, Vector3.zero, Quaternion.identity, _instance.transform);
+        menubyModule.name = modulePrefab.name;
+        _menusDictionary.Add(menubyModule.GetAssetEnum(), menubyModule);
+        // _menusDictionary[menubyModule.GetAssetEnum()].gameObject.SetActive(false);
+    }
     /// <summary>
     /// Helper Function to disable Button UI
     /// </summary>
@@ -447,5 +476,14 @@ public class MenuManager : MonoBehaviour
     public MenuCanvas GetCurrentMenu()
     {
         return _currentMainMenu;
+    }
+
+    public MenuCanvas GetMenu(AssetEnum assetEnum)
+    {
+        if (_menusDictionary.TryGetValue(assetEnum, out var menuCanvas))
+        {
+            return menuCanvas;
+        }
+        return null;
     }
 }
